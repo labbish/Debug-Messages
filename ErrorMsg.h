a6cf0e94dc101e9da614c0102bdfd15a94538b89
ZZ_404 <96425104+HZZcode@users.noreply.github.com>
2024-11-21 17:36:25 +0800

    add ErrorMsg::no_prefix()

--- ErrorMsg.h
+++ ErrorMsg.h
@@ -9,17 +9,26 @@ namespace labbish {
 		public:
 			inline ErrorMsg() :std::ostream(&buf) {
 				buf.setColor();
+				buf.setPrefix();
+			}
+			inline ErrorMsg(int) : std::ostream(&buf) {
+				buf.setColor();
 			}
 			inline ~ErrorMsg() {
 				buf.resetColor();
+			}
+			inline static ErrorMsg no_prefix() {
+				return ErrorMsg(0);
 			}
 		private:
 			class ErrorBuf : public std::streambuf {
 			public:
 				inline void setColor() {
-					std::cerr << "\033[31m[ERROR] ";
+					std::cerr << "\033[31m";
 				}
-
+				inline void setPrefix() {
+					std::cerr << "[ERROR] ";
+				}
 				inline void resetColor() {
 					std::cerr << "\033[0m\n";
 				}
