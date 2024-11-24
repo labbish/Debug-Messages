b3d0726c8f66bf8ec38b03ff419d88e73a4fdbd5
ZZ_404 <96425104+HZZcode@users.noreply.github.com>
2024-11-24 14:33:49 +0800

    simplified namespaces

--- ErrorMsg.h
+++ ErrorMsg.h
@@ -3,43 +3,41 @@
 #include <iostream>
 #include <ostream>
 
-namespace labbish {
-	namespace message {
-		class ErrorMsg :public std::ostream {
+namespace labbish::message {
+	class ErrorMsg :public std::ostream {
+	public:
+		inline ErrorMsg() :std::ostream(&buf) {
+			buf.setColor();
+			buf.setPrefix();
+		}
+		inline ErrorMsg(int) : std::ostream(&buf) {
+			buf.setColor();
+		}
+		inline ~ErrorMsg() {
+			buf.resetColor();
+		}
+		inline static ErrorMsg no_prefix() {
+			return ErrorMsg(0);
+		}
+	private:
+		class ErrorBuf : public std::streambuf {
 		public:
-			inline ErrorMsg() :std::ostream(&buf) {
-				buf.setColor();
-				buf.setPrefix();
+			inline void setColor() {
+				std::cerr << "\033[31m";
 			}
-			inline ErrorMsg(int) : std::ostream(&buf) {
-				buf.setColor();
+			inline void setPrefix() {
+				std::cerr << "[ERROR] ";
 			}
-			inline ~ErrorMsg() {
-				buf.resetColor();
+			inline void resetColor() {
+				std::cerr << "\033[0m\n";
 			}
-			inline static ErrorMsg no_prefix() {
-				return ErrorMsg(0);
+			inline int overflow(int c) override {
+				if (c != EOF) {
+					std::cerr.put(c);
+				}
+				return c;
 			}
-		private:
-			class ErrorBuf : public std::streambuf {
-			public:
-				inline void setColor() {
-					std::cerr << "\033[31m";
-				}
-				inline void setPrefix() {
-					std::cerr << "[ERROR] ";
-				}
-				inline void resetColor() {
-					std::cerr << "\033[0m\n";
-				}
-				inline int overflow(int c) override {
-					if (c != EOF) {
-						std::cerr.put(c);
-					}
-					return c;
-				}
-			};
-			ErrorBuf buf;
 		};
-	}
+		ErrorBuf buf;
+	};
 }
